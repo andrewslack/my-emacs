99426757946f1b60311eb5ab13aeb75b9478a1a5
Andrew Slack <andrew.slack@evenfinancial.com>
2023-07-01 06:26:30 -0700

    add terraform

--- init.el
+++ init.el
@@ -1093,3 +1093,19 @@ connection."
 (global-hl-line-mode +1)
 
 (set-face-background 'hl-line "gray40")
+
+;; Terraform
+(use-package terraform-mode
+  ;; if using straight
+  ;; :straight t
+
+  ;; if using package.el
+  ;; :ensure t
+  :custom (terraform-indent-level 2)
+  :config
+  (defun my-terraform-mode-init ()
+    ;; if you want to use outline-minor-mode
+    ;; (outline-minor-mode 1)
+    )
+
+  (add-hook 'terraform-mode-hook 'my-terraform-mode-init))
